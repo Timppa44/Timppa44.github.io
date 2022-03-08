99ad9304e8da958aed66c7fe1079da0491413d49
Timppa44 <timo-ringo.kukk@edu.keuda.fi>
2022-03-08 12:54:01 +0200

    moi

--- demo7/kirjautuminen.js
+++ demo7/kirjautuminen.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 function onkoKirjautunut(){
 let Kirjautunut =`kylla`;
 if(Kirjautunut === `kylla`){
-    document.getElementById("Tervetulo_teksti").textContent += `Timo!`;
+    document.getElementById("Tervetulo_teksti").textContent += localStorage.getItem("nimi");
 }
 }
 function kirjaudu(){
-    localStorage.setItem("nimi", "Timo")
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
 }
